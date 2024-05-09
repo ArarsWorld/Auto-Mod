d36625980d4915ccb759c3c358a0f6d3d8180a9c
Arab,s World <158174199+ArarsWorld@users.noreply.github.com>
2024-05-09 03:26:33 +0300

    Add files via upload

---
+++ commands/info/ping.js
@@ -0,0 +1,48 @@
+const Discord = require('discord.js');
+const {MessageEmbed} = require("discord.js");
+module.exports = {
+  name: "ping",
+  category: "🔰 Info",
+  aliases: ["latency"],
+  cooldown: 2,
+  usage: "ping",
+  description: "Gives you information on how fast the Bot can respond to you",
+  type: "bot",
+  run: async (client, message, args, cmduser, text, prefix) => {
+    
+    let es = client.settings.get(message.guild.id, "embed");let ls = client.settings.get(message.guild.id, "language")
+    try {
+      let oldate = new Date().getMilliseconds()
+      message.reply({embeds: [new MessageEmbed()
+        .setColor(es.color).setThumbnail(es.thumb ? es.footericon && (es.footericon.includes("http://") || es.footericon.includes("https://")) ? es.footericon : client.user.displayAvatarURL() : null)
+        .setFooter(client.getFooter(es))
+        .setTitle(handlemsg(client.la[ls].cmds.info.ping.m1))
+      ]}).then(msg => {
+        let newtime = new Date().getMilliseconds() - oldate;
+        if(newtime < 0) newtime*=-1;
+        msg.edit({embeds: [new MessageEmbed()
+          .setColor(es.color).setThumbnail(es.thumb ? es.footericon && (es.footericon.includes("http://") || es.footericon.includes("https://")) ? es.footericon : client.user.displayAvatarURL() : null)
+          .setFooter("It Takes longer, because i am getting my host ping!", es.footericon && (es.footericon.includes("http://") || es.footericon.includes("https://")) ? es.footericon : client.user.displayAvatarURL())
+          .setTitle(handlemsg(client.la[ls].cmds.info.ping.m2, { botping: Math.floor(client.ws.ping + newtime), ping: Math.floor(newtime) + "ms", wsping: Math.floor(client.ws.ping)}))
+        ]});
+      })
+    } catch (e) {
+      console.log(String(e.stack).grey.bgRed)
+      return message.reply({embeds: [new MessageEmbed()
+        .setColor(es.wrongcolor)
+        .setFooter(client.getFooter(es))
+        .setTitle(client.la[ls].common.erroroccur)
+        .setDescription(eval(client.la[ls]["cmds"]["info"]["color"]["variable2"]))
+      ]});
+    }
+  }
+}
+/**
+ * @INFO
+ * Bot Coded by Tomato#6966 | https://discord.gg/milrato
+ * @INFO
+ * Work for Milrato Development | https://milrato.eu
+ * @INFO
+ * Please mention him / Milrato Development, when using this Code!
+ * @INFO
+ */
